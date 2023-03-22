22fe6e83579c852486f3631c127b9d4597babe06
GabyJ <gabytaj26@gmail.com>
2023-03-22 17:00:14 -0600

    Agregando codigo a Js asi como la retroalimentacion correspondiente

--- starter-files/script.js
+++ starter-files/script.js
@@ -11,3 +11,28 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
  * 5. Optional: add loading/error states
  *
  */
+let pregunta = document.getElementById("input");
+let boton = document.getElementById("button1");
+function fetchAnswer(url) {
+    fetch(url)
+      .then (answer=> answer.json()) ;
+
+    let ubicacion = document.getElementById("answer");
+    ubicacion.innerHTML(respuesta);
+}
+
+boton.addEventListener('click', validadInput(pregunta));
+
+function limpiar(pregunta, ubicacion) {
+    setTimeout(() => {
+        pregunta.innerHTML(` `);
+        ubicacion.innerHTML(` `);
+    }, 5000);
+
+}
+function validadInput(pregunta) {
+    if (pregunta.value == "") {
+        //alert("NO hay ninguna pregunta")
+        console.log("NO hay ninguna pregunta");
+    }
+}
